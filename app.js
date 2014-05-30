b0831f1fc7e338f2e1bb288194db9947cbcf4889
181dev <181dev@users.noreply.github.com>
2014-05-30 19:39:08 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('devhub', process.env.DEVHUB);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+function postData(name, msg){
+	http.get(app.get('devhub') + "/notify?name="+name+"&msg="+msg, function(){});
+}
+
+app.post('/gitlab', function(req, res){
+	var data = req.body;
+	var msg = data.user_name + " pushes to [" + data.repository.name + "](" + data.repository.homepage + ")" ;
+	postData("gitlab", msg);
+	res.json({});
+});
+app.post('/redmine', function(req, res){
+	var data = req.body;
+	var action = data.payload.action;
+	var author = data.payload.issue.author.login;
+	var subject = data.payload.issue.subject;
+	var url = data.payload.url;
+	var msg = author + " " + action + " [" + subject + "]("+ url + ")";
+	postData("redmine", msg);
+	res.json({});
+
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
